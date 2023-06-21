928577169da7be777ca296fbb95e6b80afbdedbc
Kartavya Lathia <kart.lathia@yahoo.com>
2023-06-21 15:41:34 -0400

    Modified IPs for Vivado/Vitis 2023.1

--- src/axis2lbus/axis2lbus.cpp
+++ src/axis2lbus/axis2lbus.cpp
@@ -2,55 +2,59 @@
 Copyright (c) 2019, Qianfeng Shen
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-1. Redistributions of source code must retain the above copyright notice, 
+1. Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation 
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
 
-3. Neither the name of the copyright holder nor the names of its contributors 
-may be used to endorse or promote products derived from this software 
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
 without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.// Copyright (c) 2019, Qianfeng Shen.
 ************************************************/
 
 #include <ap_int.h>
 #include "axis2lbus.h"
 void axis2lbus(
-	AXISBUS		s_axis,
-	LBUS		lbus[4],
-	ap_uint<1>	lbus_ready,
-	ap_uint<1>	&axis_ready
-) {
-	#pragma HLS INTERFACE ap_ctrl_none port=return
-	#pragma HLS array_partition variable=lbus complete
-	#pragma HLS INTERFACE ap_none port=lbus
-	#pragma HLS INTERFACE ap_none port=s_axis
-	#pragma HLS INTERFACE ap_none port=lbus_ready
-	#pragma HLS INTERFACE ap_none port=axis_ready
+	AXISBUS s_axis,
+	LBUS lbus[4],
+	ap_uint<1> lbus_ready,
+	ap_uint<1> &axis_ready)
+{
+#pragma HLS INTERFACE ap_ctrl_none port = return
+#pragma HLS array_partition variable = lbus complete
+#pragma HLS INTERFACE ap_none port = lbus
+#pragma HLS INTERFACE ap_none port = s_axis
+#pragma HLS INTERFACE ap_none port = lbus_ready
+#pragma HLS INTERFACE ap_none port = axis_ready
+
+#pragma HLS disaggregate variable = s_axis
+#pragma HLS disaggregate variable = lbus
 
 	static ap_uint<1> IN_PACKET;
 
 	axis_ready = lbus_ready;
 
-	if (lbus_ready) {
-		lbus[0].data = s_axis.data(511,384);
-		lbus[1].data = s_axis.data(383,256);
-		lbus[2].data = s_axis.data(255,128);
-		lbus[3].data = s_axis.data(127,0);
+	if (lbus_ready)
+	{
+		lbus[0].data = s_axis.data(511, 384);
+		lbus[1].data = s_axis.data(383, 256);
+		lbus[2].data = s_axis.data(255, 128);
+		lbus[3].data = s_axis.data(127, 0);
 		lbus[0].ena = s_axis.valid;
 		lbus[1].ena = s_axis.valid & s_axis.keep[47];
 		lbus[2].ena = s_axis.valid & s_axis.keep[31];
@@ -67,16 +71,21 @@ void axis2lbus(
 		lbus[1].err = 0;
 		lbus[2].err = 0;
 		lbus[3].err = 0;
-		keep2mty(s_axis.keep(63,48),lbus[0].mty);
-		keep2mty(s_axis.keep(47,32),lbus[1].mty);
-		keep2mty(s_axis.keep(31,16),lbus[2].mty);
-		keep2mty(s_axis.keep(15,0),lbus[3].mty);
-		if (s_axis.valid & ~s_axis.last) {
+		keep2mty(s_axis.keep(63, 48), lbus[0].mty);
+		keep2mty(s_axis.keep(47, 32), lbus[1].mty);
+		keep2mty(s_axis.keep(31, 16), lbus[2].mty);
+		keep2mty(s_axis.keep(15, 0), lbus[3].mty);
+		if (s_axis.valid & ~s_axis.last)
+		{
 			IN_PACKET = 1;
-		} else if (s_axis.valid & s_axis.last) {
+		}
+		else if (s_axis.valid & s_axis.last)
+		{
 			IN_PACKET = 0;
 		}
-	} else {
+	}
+	else
+	{
 		lbus[0] = LBUS_DUMMY;
 		lbus[1] = LBUS_DUMMY;
 		lbus[2] = LBUS_DUMMY;
--- src/ether_protocol_assembler/ether_protocol_assembler.cpp
+++ src/ether_protocol_assembler/ether_protocol_assembler.cpp
@@ -2,28 +2,28 @@
 Copyright (c) 2019, Qianfeng Shen
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-1. Redistributions of source code must retain the above copyright notice, 
+1. Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation 
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
 
-3. Neither the name of the copyright holder nor the names of its contributors 
-may be used to endorse or promote products derived from this software 
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
 without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.// Copyright (c) 2019, Qianfeng Shen.
 ************************************************/
 
@@ -34,61 +34,79 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.// Copyright (c) 2019, Qianfen
 
 struct AXIS_RAW
 {
-	ap_uint<512>	data;
-	ap_uint<64>	keep;
-	ap_uint<1>	valid;
-	ap_uint<1>	last;
+	ap_uint<512> data;
+	ap_uint<64> keep;
+	ap_uint<1> valid;
+	ap_uint<1> last;
 };
 
-const struct AXIS_RAW DUMMY = {0,0,0,0};
+const struct AXIS_RAW DUMMY = {0, 0, 0, 0};
 
 void ether_protocol_assembler(
-	AXIS_RAW	eth_arp_in,
-	ap_uint<1>	&arp_ready,
-	AXIS_RAW	eth_ip_in,
-	ap_uint<1>	&ip_ready,
-	AXIS_RAW	&eth_out,
-	ap_uint<1>	eth_out_ready
-) {
-	#pragma HLS INTERFACE ap_ctrl_none port=return
-	#pragma HLS INTERFACE ap_none port=eth_arp_in
-	#pragma HLS INTERFACE ap_none port=eth_ip_in
-	#pragma HLS INTERFACE ap_none port=eth_out
-	#pragma HLS INTERFACE ap_none port=arp_ready
-	#pragma HLS INTERFACE ap_none port=ip_ready
-	#pragma HLS INTERFACE ap_none port=eth_out_ready
+	AXIS_RAW eth_arp_in,
+	ap_uint<1> &arp_ready,
+	AXIS_RAW eth_ip_in,
+	ap_uint<1> &ip_ready,
+	AXIS_RAW &eth_out,
+	ap_uint<1> eth_out_ready)
+{
+#pragma HLS INTERFACE ap_ctrl_none port = return
+#pragma HLS INTERFACE ap_none port = eth_arp_in
+#pragma HLS INTERFACE ap_none port = eth_ip_in
+#pragma HLS INTERFACE ap_none port = eth_out
+#pragma HLS INTERFACE ap_none port = arp_ready
+#pragma HLS INTERFACE ap_none port = ip_ready
+#pragma HLS INTERFACE ap_none port = eth_out_ready
+
+#pragma HLS disaggregate variable = eth_arp_in
+#pragma HLS disaggregate variable = eth_ip_in
+#pragma HLS disaggregate variable = eth_out
 
 	ap_uint<1> output_sw;
-	static ap_uint<1>	output_sw_reg;
-	static AXIS_RAW		eth_out_reg;
-	static ap_uint<1>	arbiter = 1;
+	static ap_uint<1> output_sw_reg;
+	static AXIS_RAW eth_out_reg;
+	static ap_uint<1> arbiter = 1;
 
 	eth_out = eth_out_ready ? eth_out_reg : DUMMY;
 
-	if ((eth_out_reg.valid & eth_out_reg.last) | arbiter) {
-		if (eth_arp_in.valid && output_sw_reg != ARP) {
+	if ((eth_out_reg.valid & eth_out_reg.last) | arbiter)
+	{
+		if (eth_arp_in.valid && output_sw_reg != ARP)
+		{
 			output_sw = ARP;
 			arbiter = 0;
 			output_sw_reg = ARP;
-		} else if (eth_ip_in.valid && output_sw_reg != IP) {
+		}
+		else if (eth_ip_in.valid && output_sw_reg != IP)
+		{
 			output_sw = IP;
 			arbiter = 0;
 			output_sw_reg = IP;
-		} else if (eth_arp_in.valid | eth_ip_in.valid) {
+		}
+		else if (eth_arp_in.valid | eth_ip_in.valid)
+		{
 			output_sw = output_sw_reg;
 			arbiter = 0;
-		} else {
+		}
+		else
+		{
 			output_sw = output_sw_reg;
 			arbiter = 1;
 		}
-	} else {
+	}
+	else
+	{
 		output_sw = output_sw_reg;
 	}
 
-	if (eth_out_ready) {
-		if (output_sw == ARP) {
+	if (eth_out_ready)
+	{
+		if (output_sw == ARP)
+		{
 			eth_out_reg = eth_arp_in;
-		} else {
+		}
+		else
+		{
 			eth_out_reg = eth_ip_in;
 		}
 	}
